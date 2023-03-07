143bdeac9b17a99a7240bcdd1d4fcca4cf1564e9
Fedor Borisov <102617847+FuNFoB@users.noreply.github.com>
2023-03-07 10:05:05 +0700

    Add files via upload

---
+++ PR21103k/app/src/main/java/com/example/pr_21103k/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.pr_21103k;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//Comment
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //fhjdfgjhhhhhhhhh
+    }
+}
